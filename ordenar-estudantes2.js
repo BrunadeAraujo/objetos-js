9a3869baff36acca62e866c847fd8d6a42f6d287
BrunaDeAraujo <baby.blue3940@gmail.com>
2024-07-02 22:54:33 -0300

    ordenar

---
+++ ordenar-estudantes2.js
@@ -0,0 +1,9 @@
+const months = ['March', 'Ja', 'Feb', 'Dec'];
+months.sort();
+console.log(months);
+// Expected output: Array ["Dec", "Feb", "Jan", "March"]
+
+const array1 = [1, 30, 4, 21, 100000];
+array1.sort();
+aa
+// Expected output: Array [1, 100000, 21, 30, 4]
